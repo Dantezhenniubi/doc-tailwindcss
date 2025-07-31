e8732f802e4a9ae242b119caeef90143206c22b7
xuchongzhi <983197503@qq.com>
2025-07-31 19:50:03 +0800

    添加基础文件

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,6 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitepress.vuejs.org/config/app-configs
 export default defineConfig({
+  title: "VitePress",
+  description: "VitePress 是一个基于 VuePress 的静态博客生成器",
+  base: "/doc-tailwindcss/",
   vite: {
     plugins: [tailwindcss()],
   },
