9adfa1373811bc82ad65c44e9d4cd2fc29fb3368
xuchongzhi <983197503@qq.com>
2025-08-05 17:23:48 +0800

    fix: 更改文件结构

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -9,4 +9,25 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Examples", link: "/markdown-examples" },
+    ],
+
+    sidebar: [
+      {
+        text: "Examples",
+        items: [
+          { text: "Markdown Examples", link: "/markdown-examples" },
+          { text: "Runtime API Examples", link: "/api-examples" },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+  },
 });
